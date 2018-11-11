97b9d552b2d48c5b316d05a61ed62ea15b784a2d
Richard <td231565@gmail.com>
2018-11-11 22:09:59 +0800

    xhr

--- pm25.js
+++ pm25.js
@@ -1,12 +1,37 @@
 function showView() {
-    let url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-990272/?format=xml&token=u5bfeNUmuk+Pk7tvkGpTHA",
+    let url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-990272/?format=json&token=u5bfeNUmuk+Pk7tvkGpTHA",
         view = document.getElementById("view");
-    axios.get(url)
+    let instance = axios.create({
+        baseURL: url,
+        headers: {"Access-Control-Allow-Origin": "*"}
+    })
+    axios.get(instance)
     .then(function(response){
-        view.innerHTML = this.response;
+        view.innerHTML = response;
     })
     .catch(function(){
         view.innerHTML = "error";
     });
 }
-showView();
+// showAxios();
+function showXhr(){
+    let url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-990272/?format=json&token=u5bfeNUmuk+Pk7tvkGpTHA",
+        view = document.getElementById("view");
+    let req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.onload = function(e){
+        if(req.readyState == 4){
+            if(req.status === 200){
+                console.log(req.response);
+                view.innerHTML = req.response;
+            } else {
+                console.error(req.statusText);
+            }
+        }
+    };
+    req.onerror = function(){
+        console.error(req.statusText);
+    };
+    req.send(null);
+}
+showXhr();
