250853f2fd16663147d43502cda8fd4d70683b7b
Richard <td231565@gmail.com>
2018-11-17 13:00:20 +0800

    flex content

--- pm25.js
+++ pm25.js
@@ -1,29 +1,40 @@
-function showView() {
-    let url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-990272/?format=json&token=u5bfeNUmuk+Pk7tvkGpTHA",
-        view = document.getElementById("view");
-    let instance = axios.create({
-        baseURL: url,
-        headers: {"Access-Control-Allow-Origin": "*"}
-    })
-    axios.get(instance)
-    .then(function(response){
-        view.innerHTML = response;
-    })
-    .catch(function(){
-        view.innerHTML = "error";
-    });
-}
+// function showAxios() {
+//     let url = "/text.json",
+//         view = document.getElementById("view");
+//     let instance = axios.create({
+//         baseURL: url
+//     })
+//     axios.get(instance)
+//     .then(function(response){
+//         view.innerHTML = response;
+//     })
+//     .catch(function(){
+//         view.innerHTML = "error";
+//     });
+// }
 // showAxios();
 function showXhr(){
-    let url = "https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-990272/?format=json&token=u5bfeNUmuk+Pk7tvkGpTHA",
+    let url = "https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=adef2044-760f-40bd-8e13-a7fda6d011de",
         view = document.getElementById("view");
+    // fetch (url, {method: "GET", mode: "same-origin"})
+    // .then(function(response){
+    //     if(!response.ok){
+    //         view.innerHTML = "Loaded error";
+    //     } else {
+    //         view.innerHTML = response;
+    //     }
+    // })
+    // .catch(function(){
+    //     view.innerHTML = "Error";
+    // })
     let req = new XMLHttpRequest();
     req.open("GET", url, true);
     req.onload = function(e){
         if(req.readyState == 4){
             if(req.status === 200){
-                console.log(req.response);
-                view.innerHTML = req.response;
+                let obj = JSON.parse(this.responseText);
+                view.innerHTML = obj.result.results[0];
+                view.innerHTML += this.responseText;
             } else {
                 console.error(req.statusText);
             }
