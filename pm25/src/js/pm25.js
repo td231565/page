ba282932761def4425837ddd162b9cff16780118
Richard <td231565@gmail.com>
2019-07-25 23:45:31 +0800

    new theme: dropdown menu

--- pm25/src/js/pm25.js
+++ pm25/src/js/pm25.js
@@ -1,51 +1,32 @@
-const header = document.getElementById('header')
-const view = document.getElementById('view')
-const select = document.getElementById('select')
-const card = document.createElement('div')
-const spaceBottom = document.createElement('div')
-const spaceTop = document.createElement('div')
-card.setAttribute('id', 'cardContainer')
-card.innerHTML = '<div class="hello">PM2.5</div>'
-spaceTop.setAttribute('class', 'spaceTop')
-spaceBottom.setAttribute('class', 'spaceBottom')
-view.appendChild(spaceTop)
-view.appendChild(card)
-view.appendChild(spaceBottom)
+const view = document.querySelector('.view')
+const selectBox = document.querySelector('.select-box')
+const currCity = document.querySelector('#currCity')
+const citySelect = document.querySelector('#citySelect')
+const card = document.querySelector('#cardContainer')
+const coverLayer = document.querySelector('.cover-layer')
 // options
-function showSelects(e) {
+function showCitySelectOpts(source) {
+  // console.log(source)
   // we don't want repeat options
-  let optionOrigin = []
-  let optionPicked = []
-  for (let i = 0, l = e.length; i < l; i++) {
-    optionOrigin.push(e[i].County)
-    if (optionOrigin.indexOf(optionOrigin[i]) == i) {
-      optionPicked.push(optionOrigin[i])
+  let cityOpts = []
+  source.map(item => {
+    if (!cityOpts.includes(item.County)){
+      cityOpts.push(item.County)
     }
-  }
-  // create an initial option
-  optionPicked.sort()
-  let optionInit = document.createElement('option')
-  optionInit.textContent = '請選擇'
-  optionInit.setAttribute('value', '請選擇')
-  select.appendChild(optionInit)
-  // show options
-  for (let i = 0, l = optionPicked.length; i < l; i++) {
-    let option = document.createElement('option')
-    option.textContent = optionPicked[i]
-    option.setAttribute('value', optionPicked[i])
-    select.appendChild(option)
-  }
+  })
+  cityOpts.sort().map(city => {
+    citySelect.innerHTML += `<p class="opts-city">${city}</p>`
+  })
 }
 // show pm2.5 content cards
 function showContent(json) {
-  select.addEventListener('change', function (e) {
+  citySelect.addEventListener('change', function (e) {
     // show specific card
     let value = e.target.value
     if (value !== '請選擇') {
       card.innerHTML = ''
       for (let i = 0, l = json.length; i < l; i++) {
         if (value === json[i].County)
-          // card.innerHTML += '<div class="card"><div class="cardHeader">空氣品質AQI</div><div class="cardContent">測站：' + json[i].SiteName + '<br>縣市：' + json[i].County + '<br>濃度：<span class="con"><b>' + json[i].Concentration + '</b></span> &mu;g/m<sup>3</sup> <img src="./src/img/help.png" class="helpIcon" /><br>監測時間：<br>' + json[i].MonitorDate + '<div class="help">PM2.5數值說明：<br><span class="low">[低]0~35：可正常戶外活動</span><br><span class="medium">[中]36~53：一般民眾正常戶外活動，敏感性族群建議減少戶外活動</span><br><span class="high">[高]54~70：若感到不適請減少戶外活動</span><br><span class="reallyhigh">[非常高]>71：應減少戶外活動</span></div></div></div>'
           card.innerHTML += `
           <div class="card">
             <div class="cardHeader">空氣品質AQI</div>
@@ -97,10 +78,34 @@ function getInformation() {
     .then(function (response) {
       return response.json()
     }).then(function (json) {
-      showSelects(json)
-      showContent(json)
+      showCitySelectOpts(json)
+      // showContent(json)
+      coverLayer.textContent = '讀取完畢！'
+      coverLayer.classList.add('hide')
     }).catch(function (err) {
       view.innerHTML = err
     })
 }
 getInformation()
+// 建立 loader page 的 DOM
+window.addEventListener('DOMContentLoaded', () => {
+  let loaderAnimation = document.querySelector('.loader')
+  for (let i=0; i<5; i++){
+    let dot = document.createElement('div')
+    dot.className = 'dot'
+    loaderAnimation.appendChild(dot)
+  }
+})
+// 關聯 city select
+window.addEventListener('click', (e) => {
+  // console.log(e.target)
+  if (e.target.id === 'currCity' || e.target.className === 'select-box'){
+    selectBox.classList.toggle('active')
+  } else {
+    selectBox.classList.remove('active')
+  }
+  if (e.target.className === 'opts-city'){
+    currCity.textContent = e.target.textContent
+    selectBox.classList.remove('active')
+  }
+})
